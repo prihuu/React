c9b4e030d895908c79186b460f2d9d5d538e05d0
awahn <awahnde.princess@gmail.com>
2026-09-23 00:40:08 +0300

    Complete hooks lab assignment 1

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,4 +1,24 @@
+import {useState, useEffect} from 'react';
+import {fetchData} from '../utils/fetchData';
+
 const Home = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+
+  useEffect(() => {
+    const getMedia = async () => {
+      try {
+        const json = await fetchData('test.json');
+        setMediaArray(json);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getMedia();
+  }, []);
+
+  console.log(mediaArray);
+
   return (
     <>
       <h1>Home</h1>
