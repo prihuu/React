acf40864ceac4a9df0806dcf503f008c9b27fa69
awahn <awahnde.princess@gmail.com>
2026-09-22 22:32:35 +0300

    Complete React Router

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -14,6 +14,9 @@ const Layout = () => {
           <li>
             <Link to="/upload">Upload</Link>
           </li>
+          <li>
+            <Link to="/single">Single</Link>
+          </li>
         </ul>
       </nav>
 
